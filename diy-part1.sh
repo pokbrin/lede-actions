1b680abb1adf19562c1661d4487f8372ccbb675b
pokbrin <pokbrin@gmail.com>
2023-02-03 16:29:15 +0800

    modify diy.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo "src-git vernesong https://github.com/vernesong/OpenClash.git"  >> feeds.conf.default
+echo "src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package" >> feeds.conf.default
